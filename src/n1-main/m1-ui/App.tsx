dab9e9fb68d2f19fa20fa53d5cc11d37722925f1
trishinanton <tosha.trishin@bk.ru>
2021-10-14 22:31:32 +0300

    init com

--- src/n1-main/m1-ui/App.tsx
+++ src/n1-main/m1-ui/App.tsx
@@ -3,16 +3,19 @@ import logo from '../../logo.svg';
 import './App.css';
 import {Provider} from "react-redux";
 import {HashRouter} from "react-router-dom";
+import {Header} from "./header/Header";
+import {Routes} from "./routes/Routes";
+import {store} from "../m2-bll/b1-srore/redux-store";
 
 function App() {
   return (
     <div className="App">
-      {/*<Provider store={store}>*/}
+      <Provider store={store} >
         <HashRouter>
-          {/*<Header/>*/}
-          {/*<Main/>*/}
+          <Header/>
+          <Routes/>
         </HashRouter>
-      {/*</Provider>*/}
+      </Provider>
     </div>
   );
 }
